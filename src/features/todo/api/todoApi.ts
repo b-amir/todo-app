b14e633b8483cf60484ac5993f1709d01a829c63
theamirm <theamirm@gmail.com>
2025-08-16 22:20:27 +0330

    feat: implement todo API functions for CRUD operations

---
+++ src/features/todo/api/todoApi.ts
@@ -0,0 +1,38 @@
+import apiClient from "@/src/shared/utils/apiClient";
+import type {
+  CreateTodoRequest,
+  DeletedTodo,
+  Todo,
+  TodosResponse,
+  UpdateTodoRequest,
+} from "./types";
+
+export const fetchTodos = ({
+  limit = 20,
+  skip = 0,
+}: { limit?: number; skip?: number } = {}): Promise<TodosResponse> => {
+  return apiClient(`todos?limit=${limit}&skip=${skip}`);
+};
+
+export const createTodo = (newTodo: CreateTodoRequest): Promise<Todo> => {
+  return apiClient("todos/add", {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+  });
+};
+
+export const updateTodo = ({
+  id,
+  ...patch
+}: UpdateTodoRequest): Promise<Todo> => {
+  return apiClient(`todos/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(patch),
+  });
+};
+
+export const deleteTodo = (id: number): Promise<DeletedTodo> => {
+  return apiClient(`todos/${id}`, {
+    method: "DELETE",
+  });
+};
