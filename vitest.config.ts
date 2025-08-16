3d58b8be1a5b377ef62c5bafdd03306e895d9515
theamirm <theamirm@gmail.com>
2025-08-16 22:47:51 +0330

    chore: fix Vitest configuration and setup files for testing environment

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import path from "path";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
+});
